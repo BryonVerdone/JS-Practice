ad1407173121489d9898f9e204c592b9b0c2a36d
BryonVerdone <bryonverdone@gmail.com>
2022-02-28 21:09:34 -0500

    added fizz buzz and other looping exercises

--- app.js
+++ app.js
@@ -161,6 +161,48 @@ btn.addEventListener('click', function () {
   text.classList.add('taco');
 });
 
-for (let taco = 0; taco <= 10; taco++) {
-  console.log(taco);
+// for (let taco = 0; taco <= 10; taco++) {
+//   console.log(taco);
+// }
+
+for (let i = 1; i < 101; i++) {
+  if (i % 15 == 0) console.log('FizzBuzz');
+  else if (i % 3 == 0) console.log('Fizz');
+  else if (i % 5 == 0) console.log('Buzz');
+  else console.log(i);
 }
+
+// Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+// When it's done, improve it so that the number of turns is given by the user.
+
+// let i;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Turn number: ${i}`);
+// }
+
+// let i = parseInt(prompt("How many turns on the carousel?"));
+let num = prompt('How many turns on the carousel?');
+for (let i = 1; i <= num; i++) {
+  console.log(`Turn ${num}`);
+}
+
+//Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
+
+// let i;
+// for (let i = 0; i <= 100; i++) {
+//   i = prompt("Give me a number please");
+//   if (i <= 100) {
+//     alert("you did it");
+//   }
+// }
+
+// let number = prompt('give me a number 50-100');
+
+// while (number <= 50 || number >= 100) {
+//   number = prompt('give me a number 50-100');
+// }
+
+// Write a program that asks the user for a number, then shows the multiplication table for this number.
+
+// When you are done, improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).
